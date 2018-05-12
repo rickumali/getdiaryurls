e505d99e15b740b3650541ffcfbeee9a09139b65
Rick Umali <rickumali@gmail.com>
2018-05-12 16:26:42 -0400

    Fix to remove empty days (again)
    This commit is a better fix than the last commit. If there are no URLs,
    then NO year/month/day bin should be created.

--- getdiaryurls.py
+++ getdiaryurls.py
@@ -18,21 +18,21 @@ ary.sort()
 year_buckets = {}
 
 for filename in ary:
-	date, ext = filename.split('.')
-	if len(date) != 8:
-		print "Problem: %s" % filename
-		next
-	datefmt = compile('(?P<year>[1-2][0-9][0-9][0-9])(?P<month>[0-9][0-9])(?P<day>[0-9][0-9])')
-	year = datefmt.search(date).group('year')
-	month = datefmt.search(date).group('month')
-	day = datefmt.search(date).group('day')
-	if year not in year_buckets:
-		year_buckets[year] = {}
-	if month not in year_buckets[year]:
-		year_buckets[year][month] = {}
-	if day not in year_buckets[year][month]:
-		f = FileURLs(diary_dir + "/" + filename)
-		if len(f.urls()) > 0:
+	f = FileURLs(diary_dir + "/" + filename)
+	if len(f.urls()) > 0:
+		date, ext = filename.split('.')
+		if len(date) != 8:
+			print "Problem: %s" % filename
+			next
+		datefmt = compile('(?P<year>[1-2][0-9][0-9][0-9])(?P<month>[0-9][0-9])(?P<day>[0-9][0-9])')
+		year = datefmt.search(date).group('year')
+		month = datefmt.search(date).group('month')
+		day = datefmt.search(date).group('day')
+		if year not in year_buckets:
+			year_buckets[year] = {}
+		if month not in year_buckets[year]:
+			year_buckets[year][month] = {}
+		if day not in year_buckets[year][month]:
 			year_buckets[year][month][day] = f.urls()
 
 o = NestedHTML(year_buckets)
